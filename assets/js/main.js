294f8604e30d91b98cc889350e7bf5ed5049004f
bvasic <vasicbie@gmail.com>
2020-04-01 15:08:47 +0700

    Iterating API endpoint to wp frontend

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
+
     console.log("There it is!");
+
+        $('#usersList').DataTable( {
+            ajax: {
+                url: 'http://localhost/inpsyde/fetch.php',
+                dataSrc: ''
+            },
+            columns: [ 
+                        { data: 'name' },
+                        { data: 'email' },
+                        { data: 'address.street' },
+                        { data: 'address.zipcode'}
+                    ]
+        } );
+
 });
--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -13,8 +13,10 @@
 
     function enqueue(){
         //enqueue all scripts
-        wp_enqueue_style( 'pluginStyle', $this->plugin_url . ( '/assets/css/style.css' ) );
+        wp_enqueue_style( 'pluginStyle', $this->plugin_url . ( '/assets/css/style.css' ) );   
         wp_enqueue_script( 'pluginScript', $this->plugin_url . ( '/assets/js/jquery-3.4.1.min.js' ) );
+        wp_enqueue_style( 'pluginDataTableStyle', $this->plugin_url . ( '/assets/css/jquery.dataTables.min.css' ) );
+        wp_enqueue_script( 'pluginDataTablesScript', $this->plugin_url . ( '/assets/js/jquery.dataTables.min.js' ) );
         wp_enqueue_script( 'pluginCustomScript', $this->plugin_url . ( '/assets/js/main.js' ) );
     }
   
